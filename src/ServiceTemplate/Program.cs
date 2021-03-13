35282feca2b0ce3db21056199872754a74bc04bf
Gab Soulavy <=>
2021-03-13 21:36:20 +0000

    initial
    1517510+gsoulavy@users.noreply.github.com

---
+++ src/ServiceTemplate/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace ServiceTemplate
+{
+    using System;
+
+    internal class Program
+    {
+        private static void Main(string[] args) => Console.WriteLine("Hello World!");
+    }
+}
