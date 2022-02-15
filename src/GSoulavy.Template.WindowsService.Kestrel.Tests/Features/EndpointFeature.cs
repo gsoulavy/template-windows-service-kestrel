e3b50997c42f14ff53122c940dc469cf50f2be1b
Gab Soulavy <1517510+gsoulavy@users.noreply.github.com>
2022-02-15 16:27:43 +0000

    add dotnet 6 support with minimal

--- src/GSoulavy.Template.WindowsService.Kestrel.Tests/Features/EndpointFeature.cs
+++ src/GSoulavy.Template.WindowsService.Kestrel.Tests/Features/EndpointFeature.cs
@@ -1,24 +1,23 @@
-﻿namespace GSoulavy.Template.WindowsService.Kestrel.Tests.Features
+﻿namespace GSoulavy.Template.WindowsService.Kestrel.Tests.Features;
+
+using AutoFixture.Xunit2;
+
+using Fixtures;
+
+using Xbehave;
+
+public class EndpointFeature
 {
-    using AutoFixture.Xunit2;
-
-    using Fixtures;
-
-    using Xbehave;
-
-    public class EndpointFeature
+    [Scenario]
+    [AutoData]
+    public void AcceptsRequest(EndpointFixture fixture)
     {
-        [Scenario]
-        [AutoData]
-        public void AcceptsRequest(EndpointFixture fixture)
-        {
-            var name = "John";
-            var expected = $"{name} accepted";
-            "GIVEN the controller is running".x(fixture.GivenTheControllerIsRunning);
-            $"WHEN the endpoint is called with {name}".x(() => fixture.WhenTheEndpointIsCalledWith(name));
-            "THEN returns accepted".x(fixture.ThenReturnsAccepted);
-            "AND the content is ResponseRoot".x(fixture.AndTheContentIsResponseRoot);
-            $"AND the result contains {expected}".x(() => fixture.AndTheResultContains(expected));
-        }
+        var name = "John";
+        var expected = $"{name} accepted";
+        "GIVEN the controller is running".x(fixture.GivenTheControllerIsRunning);
+        $"WHEN the endpoint is called with {name}".x(() => fixture.WhenTheEndpointIsCalledWith(name));
+        "THEN returns accepted".x(fixture.ThenReturnsAccepted);
+        "AND the content is ResponseRoot".x(fixture.AndTheContentIsResponseRoot);
+        $"AND the result contains {expected}".x(() => fixture.AndTheResultContains(expected));
     }
 }
